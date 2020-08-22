54274a658259b5286cc081cc151246328772c9e4
Stephen Woosley <stephenrwoosley@gmail.com>
2020-08-22 10:38:20 -0400

    fix util error

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,14 +3,13 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  axios.get("localhost:3001/api/allPeople")
-  // getRandomDog: function() {
-  //   return axios.get("https://dog.ceo/api/breeds/image/random");
-  // },
-  // getDogsOfBreed: function(breed) {
-  //   return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  // },
-  // getBaseBreedsList: function() {
-  //   return axios.get("https://dog.ceo/api/breeds/list");
-  // }
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
+  }
 };
