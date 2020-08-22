014a755fcc5fbe9c1de33a954772f645543fdfed
Stephen Woosley <stephenrwoosley@gmail.com>
2020-08-22 10:38:49 -0400

    Updates

---
+++ precache-manifest.6a0b0416029b99842bd8a9710f00ed68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252badffe5e9c1457949533ed0796294",
+    "url": "/gh-pages-react-deploy-test/index.html"
+  },
+  {
+    "revision": "28fde39a02f5e31ce06d",
+    "url": "/gh-pages-react-deploy-test/static/css/main.d9a7a9b8.chunk.css"
+  },
+  {
+    "revision": "91301f9f5839fded78e6",
+    "url": "/gh-pages-react-deploy-test/static/js/2.8b579a12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gh-pages-react-deploy-test/static/js/2.8b579a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28fde39a02f5e31ce06d",
+    "url": "/gh-pages-react-deploy-test/static/js/main.896826b2.chunk.js"
+  },
+  {
+    "revision": "9fc0a149f40b79c8b9bb",
+    "url": "/gh-pages-react-deploy-test/static/js/runtime-main.ef5add80.js"
+  }
+]);
